86b6e3eeb9bf53477e974c98829082f7179ede8f
abhishek232005 <abhishek232005@gmail.com>
2026-05-10 13:44:31 +0530

    fix typescript splittext

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -5,22 +5,27 @@ import { SplitText } from "gsap-trial/SplitText";
 
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
-  split?: SplitText;
+  split?: any;
 }
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
+
   if (window.innerWidth < 900) return;
+
   const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
+
   const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
   const TriggerStart = window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+
   const ToggleAction = "play pause resume reverse";
 
-  paras.forEach((para: ParaElement) => {
+  paras.forEach((para) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -32,7 +37,7 @@ export default function setSplitText() {
     });
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para.split?.words || [],
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -45,20 +50,23 @@ export default function setSplitText() {
         ease: "power3.out",
         y: 0,
         stagger: 0.02,
-      }
+      },
     );
   });
-  titles.forEach((title: ParaElement) => {
+
+  titles.forEach((title) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title.split?.chars || [],
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
@@ -72,7 +80,7 @@ export default function setSplitText() {
         y: 0,
         rotate: 0,
         stagger: 0.03,
-      }
+      },
     );
   });
 
